d8991e9d3a83a9b6ae5bd14c4c63dfd9927e0b1b
lyxose <lyxose1097@sjtu.edu.cn>
2024-01-03 01:42:11 +0800

    modified the annotations

--- create_video.py
+++ create_video.py
@@ -17,14 +17,12 @@ def frame2video(path='_temp_VideoData',vidname='output'):
     print(size)
     fourcc = cv2.VideoWriter_fourcc(*"DVIX")
     videoWrite = cv2.VideoWriter(f'{vidname}.mp4',fourcc,fps,size)# 根据图片的大小，创建写入对象 （文件名，支持的编码器，5帧，视频大小（图片大小））
-    #videoWrite = cv2.VideoWriter('0.mp4',fourcc,fps,(1920,1080))
 
     files = os.listdir(f'{path}/')
     out_num = len(files)
     for i in range(0,out_num):
         fileName = f'{path}/'+str(i)+'.jpg'    #循环读取所有的图片,假设以数字顺序命名
         img = cv2.imread(fileName)
-        # print(img.shape[1],img.shape[0])
         videoWrite.write(img)# 将图片写入所创建的视频对象
     videoWrite.release()
 # %%
