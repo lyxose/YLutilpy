934309e4efdfd11248c574c20fd216e4572ec719
lyxose <lyxose1097@sjtu.edu.cn>
2024-01-03 16:17:00 +0800

    add the url and version

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='YLutilpy',
-    version=1.0,
+    version='0.0.1',
     description='Some handy functions for Python data analysis',
     author='Yu-Xin Lu',
     author_email='1870562999@qq.com',
+    url="https://github.com/lyxose/YLutilpy",
     packages=find_packages()
 )
