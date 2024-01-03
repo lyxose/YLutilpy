fff63ab9d416462cc2ed969d0fc3bdc1d624879f
lyxose <lyxose1097@sjtu.edu.cn>
2024-01-03 14:18:18 +0800

    add the setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='YLutilpy',
+    version=1.0,
+    description='Some handy functions for Python data analysis',
+    author='Yu-Xin Lu',
+    author_email='1870562999@qq.com',
+    packages=find_packages()
+)
